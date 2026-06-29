0be8aa0a0975db071c54e6676ab0dd4b6e6e9338
Joseph John <joseph.john@anu.edu.au>
2026-06-29 11:34:05 +1000

    exercise: change code to montecarlo with timimg

--- exercises/4_set_target.c
+++ exercises/4_set_target.c
@@ -1,20 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 
 int main(void)
 {
+    const long N = 100000000;
+
+    long inside = 0;
+
+    double start = omp_get_wtime();
+
     printf("Default device: %d\n", omp_get_default_device());
 
-    #pragma omp target device(1)
+    #pragma omp target device(1) map(tofrom:inside)
     {
+
         printf("Inside target region\n");
         printf("Device num (inside target): %d\n", omp_get_device_num());
-
-        for (int i = 0; i < 10; i++)
+        
+        for (long i = 0; i < N; i++)
         {
-            printf("%d\n", i);
+            unsigned int seed = (unsigned int)i;
+
+            float x =
+                (float)rand_r(&seed) /
+                (float)RAND_MAX;
+
+            float y =
+                (float)rand_r(&seed) /
+                (float)RAND_MAX;
+
+            if (x * x + y * y <= 1.0f)
+            {
+                inside++;
+            }
         }
     }
+
+    double end = omp_get_wtime();
+
+    printf("Pi ~= %.10f\n",
+           4.0 * (double)inside / (double)N);
+
+    printf("Execution Time = %.6f s\n",
+           end - start);
 
     return 0;
 }
